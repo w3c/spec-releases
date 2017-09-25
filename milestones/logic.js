182452a5fa4421a7d6be8de7bbb1b2541b3d9537
plehegar <plh@w3.org>
2017-09-25 16:59:09 -0400

    Avoid FF console warning

--- milestones/logic.js
+++ milestones/logic.js
@@ -319,7 +319,9 @@
        var result = {};
        query.split("&").forEach(function(part) {
         var item = part.split("=");
-        result[item[0]] = decodeURIComponent(item[1]);
+        if (item[1] !== undefined) {
+          result[item[0]] = decodeURIComponent(item[1]);
+        }
        });
       return result;
     }
